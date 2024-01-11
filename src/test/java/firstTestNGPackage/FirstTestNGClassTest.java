a7d29ab55114ee8f12f0e84a91ba4b9499d75126
kajali1 <kajaliagrawal4@gmail.com>
2024-01-11 16:13:57 +0000

    Adding chromeoptions package

--- src/test/java/firstTestNGPackage/FirstTestNGClassTest.java
+++ src/test/java/firstTestNGPackage/FirstTestNGClassTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.AssertJUnit;
 import org.testng.annotations.AfterMethod;
